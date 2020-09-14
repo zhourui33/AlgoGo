71f3f4e4e2aafc20abd811fa46f5a9aede90a0cc
zhourui <xxxx@163.com>
2020-09-14 22:43:59 +0800

    support linklist loop detect & mid node get

--- include/LinkList.h
+++ include/LinkList.h
@@ -1,14 +1,14 @@
 /**
 
 TODO: 
-1.linklist 实现编译通过
+1.linklist 实现编译通过 ok
 2.实现回文判断 palindrome
 3.实现LRU缓存淘汰队列
-4.链表逆置
-5.链表中环的检测
+4.链表逆置 ok
+5.链表中环的检测 ok
 6.两个有序链表合并
 7.删除链表倒数第N个节点
-8.求链表中间节点
+8.求链表中间节点 ok
 
 **/
 
@@ -33,10 +33,18 @@ struct LinkList
     LinkList();
     virtual ~LinkList();
 
+    bool empty() const;
     void insert(const T&);
-    void reverse();
-    void show();
+    LinkList<T>& reverse();
+    LinkNode<T>* getMidNode();
+    bool isExistLoop();
+    bool isPalindrome();
+    bool operator==(const LinkList<T>&) const;
 
+    void show() const;
+
+    private:
+    void destroy();
     private:
     LinkNode<T> *_head;
 };
@@ -50,6 +58,12 @@ inline LinkList<T>::LinkList()
 
 template<typename T>
 inline LinkList<T>::~LinkList()
+{
+    destroy();
+}
+
+template<typename T>
+void LinkList<T>::destroy()
 {
     while(_head)
     {
@@ -69,7 +83,7 @@ void LinkList<T>::insert(const T &data)
 }
 
 template<typename T>
-void LinkList<T>::show()
+void LinkList<T>::show() const
 {
     LinkNode<T> *temp = _head->next;
     while(temp)
@@ -84,7 +98,7 @@ void LinkList<T>::show()
     [head|next]->[A|next]->[B|next]->[C|null]
 */
 template<typename T>
-void LinkList<T>::reverse()
+LinkList<T>& LinkList<T>::reverse()
 {
     LinkNode<T> *cur = _head->next, 
                 *prev = nullptr, 
@@ -102,6 +116,94 @@ void LinkList<T>::reverse()
     }
     // 4. link head node to first node after reverse
     _head->next = prev;
+    return *this;
+}
+
+template<typename T>
+bool LinkList<T>::operator==(const LinkList<T>& rhs) const
+{
+    if(empty() || rhs.empty()) 
+    {
+        return false;
+    }
+    
+    LinkNode<T> *rnode = _head->next, 
+                *lnode = rhs._head->next;
+    while(lnode && rnode)
+    {
+        if(lnode->data != rnode->data)
+        {
+            return false;
+        }
+        lnode = lnode->next;
+        rnode = rnode->next;
+    }
+    if(lnode || rnode)
+    {
+        return false;
+    }
+    return true;
+}
+
+template<typename T>
+bool LinkList<T>::empty() const
+{
+    return (_head->next == nullptr);
+}
+
+template<typename T>
+bool LinkList<T>::isExistLoop()
+{
+    if(empty())
+    {
+        return false;
+    }
+        
+    LinkNode<T> *fast = _head, *slow = _head;
+    bool loopExist = false;
+    while(fast && fast->next)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+        if(fast == slow)
+        {
+            loopExist = true;
+            break;
+        }
+    }
+    return loopExist;
+}
+
+
+template<typename T>
+bool LinkList<T>::isPalindrome()
+{
+    if(empty())
+    {
+        return false;
+    }
+
+
+    return false;
+}
+
+template<typename T>
+LinkNode<T>* LinkList<T>::getMidNode()
+{
+    if(empty() || isExistLoop())
+    {
+        exit(1);
+    }
+    LinkNode<T> *fast = _head->next, 
+                *slow = _head->next;
+
+    while(fast && fast->next)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow;
+    
 }
 
 #endif
--- test/test_linklist.cpp
+++ test/test_linklist.cpp
@@ -1,30 +1,104 @@
 #include <gtest/gtest.h>
 #include "LinkList.h"
+#include <string>
 
-TEST(test_link_list, should_link_list_insert_behind_head)
+using namespace std;
+
+TEST(test_link_list, should_link_list_int_insert_as_expect)
 {
     LinkList<int> mon;
     mon.insert(1);
-    mon.insert(2);
-    mon.insert(3);
-    mon.insert(4);
-    mon.insert(5);
-    mon.insert(6);
-    mon.show();
+
+    ASSERT_FALSE(mon.empty());
+}
+
+TEST(test_link_list, should_link_list_string_insert_as_expect)
+{
+    LinkList<string> mon;
+    mon.insert("january");
+    
+    ASSERT_FALSE(mon.empty());
+}
+
+TEST(test_link_list, should_same_link_list_cmp_equal)
+{
+    LinkList<int> left, right;
+    left.insert(1);
+    right.insert(1);
+
+    ASSERT_TRUE(left == right);
 }
 
 TEST(test_link_list, should_link_list_reverse_as_expect)
 {
-    LinkList<int> mon;
-    mon.insert(1);
-    mon.insert(2);
-    mon.insert(3);
-    mon.insert(4);
-    mon.insert(5);
-    mon.insert(6);
-    mon.show();
-    mon.reverse();
-    mon.show();
-    mon.reverse();
-    mon.show();
+    LinkList<int> origin, reverse;
+    origin.insert(1);
+    origin.insert(2);
+    origin.insert(3);
+
+    reverse.insert(3);
+    reverse.insert(2);
+    reverse.insert(1);
+    ASSERT_TRUE(origin.reverse() == reverse);
+}
+
+TEST(test_link_list, should_detect_loop_when_link_list_exist_loop)
+{
+    LinkList<int> noloop;
+    noloop.insert(1);
+    noloop.insert(2);
+
+    ASSERT_FALSE(noloop.isExistLoop());
+}
+
+TEST(test_link_list, should_get_mid_node_when_list_has_odd_nodes)
+{
+    LinkList<string> oddnodes;
+    oddnodes.insert("one");
+    oddnodes.insert("two");
+    oddnodes.insert("three");
+    oddnodes.insert("four");
+    oddnodes.insert("five");
+
+    LinkNode<string> *midnode = oddnodes.getMidNode();
+    ASSERT_TRUE(midnode->data == "three");
+}
+
+TEST(test_link_list, should_get_mid_node_when_list_has_even_nodes)
+{
+    LinkList<string> evennodes;
+    evennodes.insert("one");
+    evennodes.insert("two");
+    evennodes.insert("three");
+    evennodes.insert("four");
+    evennodes.insert("five");
+    evennodes.insert("six");
+
+    LinkNode<string> *midnode = evennodes.getMidNode();
+    cout<<midnode->data<<endl;
+    ASSERT_TRUE(midnode->data == "three");
+}
+
+TEST(test_link_list, should_detect_link_list_is_palindrome)
+{
+    LinkList<string> palindrome;
+    palindrome.insert("did");
+    palindrome.insert("anna");
+    palindrome.insert("say");
+    palindrome.insert("as");
+    palindrome.insert("anna");
+    palindrome.insert("did");
+    
+    ASSERT_TRUE(palindrome.isPalindrome());
+}
+
+TEST(test_link_list, should_detect_link_list_is_not_palindrome)
+{
+    LinkList<string> palindrome;
+    palindrome.insert("i");
+    palindrome.insert("am");
+    palindrome.insert("not");
+    palindrome.insert("palindrome");
+    
+    ASSERT_FALSE(palindrome.isPalindrome());
 }
